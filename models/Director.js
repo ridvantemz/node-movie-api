598c2aaab6af4764720bb04908d6a01d9e498b97
Rıdvan Temiz <ridvantemizz@gmail.com>
2018-07-01 19:05:33 +0300

    kayıt sırasındaki validation düzenlenmesi

--- models/Director.js
+++ models/Director.js
@@ -3,9 +3,21 @@ const Schema=mongoose.Schema;
 
 const DirectorSchema=new Schema({
     
-    name:String,
-    surname:String,
-    bio:String,
+    name:{
+        type:String,
+        maxlength:[100,'`{PATH}` alanı (`{VALUE}`), ({MAXLENGTH}) karakterden daha az olmalıdır.'],
+        minlength:[3,'`{PATH}` alanı (`{VALUE}`), ({MINLENGTH}) karakterden daha fazla olmalıdır.']
+    },
+    surname:{
+        type:String,
+        maxlength:[100,'`{PATH}` alanı (`{VALUE}`), ({MAXLENGTH}) karakterden daha az olmalıdır.'],
+        minlength:[2,'`{PATH}` alanı (`{VALUE}`), ({MINLENGTH}) karakterden daha fazla olmalıdır.']
+    },
+    bio:{
+        type:String,
+        maxlength:[500,'`{PATH}` alanı (`{VALUE}`), ({MAXLENGTH}) karakterden daha az olmalıdır.'],
+        minlength:[10,'`{PATH}` alanı (`{VALUE}`), ({MINLENGTH}) karakterden daha fazla olmalıdır.']
+    },
     createdAt:{
         type:Date,
         default:Date.now

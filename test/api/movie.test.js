fdfeded49bf06eda5d89d3b64c8c2757ab5c1477
Rıdvan Temiz <ridvantemizz@gmail.com>
2018-07-02 19:39:27 +0300

    /GET/:movie_id movie test

--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -7,7 +7,7 @@ const server =require('../../app');
 
 chai.use(chaiHttp);
 
-let token;
+let token,movieId;
 
 describe('/api/movies tests',()=>{
    before((done)=>{
@@ -16,7 +16,7 @@ describe('/api/movies tests',()=>{
         .send({username:'ridvan', password:'123456789'})
         .end((err,res)=>{
            token=res.body.token;
-           console.log(token);
+           //console.log(token);
            done();
        })
    });
@@ -32,6 +32,60 @@ describe('/api/movies tests',()=>{
                done();
         })
        })
-   })
-   
+   });
+    
+    describe('/POST movie',()=>{
+        it('it should POST a movie',(done)=>{
+            const movie={
+                title:'Udemy',
+                director_id:'5b38ec71807c1f8331447905',
+                category:'comedi',
+                country:'Turkey',
+                year:1950,
+                imdb_score:8
+            };
+            chai.request(server)
+            .post('/api/movies')
+            .send(movie)
+            .set('x-access-token',token)
+            .end((err,res)=>{
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('title');
+                res.body.should.have.property('director_id');
+                res.body.should.have.property('category');
+                res.body.should.have.property('country');
+                res.body.should.have.property('year');
+                res.body.should.have.property('imdb_score');
+                movieId=res.body._id;
+                done();
+            })
+            
+        })
+    });
+    
+    
+    
+    describe('/GET/:director_id movie',()=>{
+        it('it should GET a movie by the given id', (done)=>{
+            chai.request(server)
+            .get('/api/movies/'+movieId)
+            .set('x-access-token',token)
+            .end((err,res)=>{
+                res.should.have.status(200);
+                res.body.should.be.a('object')
+                res.body.should.have.property('title');
+                res.body.should.have.property('director_id');
+                res.body.should.have.property('category');
+                res.body.should.have.property('country');
+                res.body.should.have.property('year');
+                res.body.should.have.property('imdb_score');
+                res.body.should.have.property('_id').eql(movieId);
+                done();
+
+            })
+        })
+    })
+    
+    
 })

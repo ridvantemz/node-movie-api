60c24956366192fbc49b501fd93efb9a29b4623b
Rıdvan Temiz <ridvantemizz@gmail.com>
2018-07-02 19:45:34 +0300

    /PUT/:director_id movie test script

--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -85,7 +85,38 @@ describe('/api/movies tests',()=>{
 
             })
         })
-    })
+    });
+    
+    describe('/PUT/director_id movie',()=>{
+        it('it should PUT a movie given by id',(done)=>{
+            const movie={
+                title:'93creative',
+                director_id:'5b38ec71807c1f8331447915',
+                category:'crime',
+                country:'Franse',
+                year:1993,
+                imdb_score:6
+            };
+            chai.request(server)
+            .put('/api/movies/'+movieId)
+            .send(movie)
+            .set('x-access-token',token)
+            .end((err,res)=>{
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('title').eql(movie.title);
+                res.body.should.have.property('director_id').eql(movie.director_id);
+                res.body.should.have.property('category').eql(movie.category);
+                res.body.should.have.property('country').eql(movie.country);
+                res.body.should.have.property('year').eql(movie.year);
+                res.body.should.have.property('imdb_score').eql(movie.imdb_score);
+                done();
+            })
+            
+        })
+    });
+    
+    
     
     
 })
